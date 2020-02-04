a2e3c9360cd6e71afec47a09367bbfb611ec37ac
maxastuart <maxastuart@gmail.com>
2020-02-04 01:19:13 +0000

    Got basic GET request working

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -2,3 +2,8 @@
 # for a given employee ID, returns information about his/her TODO list progress.
 # from REST API at https://jsonplaceholder.typicode.com/
 
+import requests
+from sys import argv
+if __name__ == "__main__":
+    r = requests.get('https://jsonplaceholder.typicode.com/users/{}'.format(argv[1]))
+    print(r.json())
