d0f8afccb22223f70259b11fc3b4cf3ad8a3128c
Max Stuart <maxastuart@gmail.com>
2020-02-07 02:39:55 +0000

    Create dir and readme and 0

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+# Defines number_of_subscribers function
+
+import requests as r
+
+
+def number_of_subscribers(subreddit):
+    """
+    queries the Reddit API for a subreddit's number of active subscribers.
+
+    return(int): number of subreddit's subscribers, 0 if subreddit invalid
+    """
+    return r.get(
+        'https://api.reddit.com/r/{}/about'.format(subreddit), headers={
+            'user-agent': 'python:v3.5.2 (by /u/maxastuart)'
+        }).json().get('data').get('subscribers')
---
+++ 0x16-api_advanced/README.md
@@ -0,0 +1,3 @@
+# 0x16-api_advanced
+
+## Max Stuart 2020 Feburary 06
